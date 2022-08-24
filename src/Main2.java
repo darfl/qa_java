5fed3445e55bbd3c92cc54e8ed2e4020135d7301
darfl <diana.darfl@gmail.com>
2022-08-25 01:06:18 +0500

    dz2.1

---
+++ src/Main2.java
@@ -0,0 +1,12 @@
+public class Main2 {
+
+    public static void main (String[] args) {
+
+        int ticketPrice = 10299;
+        int everyTwentyRub = 20;
+
+        int miles = ticketPrice / everyTwentyRub;
+
+        System.out.println(miles);
+    }
+}
